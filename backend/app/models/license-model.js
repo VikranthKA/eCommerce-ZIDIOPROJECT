a124a85727845cfff440d295274587e838c2e9bb
vikranth <vikranthka@gmail.com>
2024-07-16 15:38:40 +0530

    added the license model

---
+++ backend/app/models/license-model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const licenseSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    productId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+    },
+    licenseKey: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    expiryDate: {
+        type: Date,
+    },
+}, { timestamps: true });
+
+const License = model('License', licenseSchema);
+module.exports = License;
--- backend/app/models/product-model.js
+++ backend/app/models/product-model.js
@@ -1,5 +1,58 @@
-const mongoose = require("mongoose")
-const {Schema,model} = mongoose
+// const mongoose = require("mongoose")
+// const {Schema,model} = mongoose
+
+// const productSchema = new Schema({
+//     userId: {
+//         type: String,
+//         required: true,
+//     },
+//     name: {
+//         type: String,
+//         required: true,
+//     },
+//     categoryId: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'Category',
+//     },
+//     description: {
+//         type: String,
+//         required: true,
+//     },
+//     price: {
+//         type: Number,
+//         default: 0,
+//     },
+//     currency: {
+//         type: String,
+//         default: 'INR',
+//     },
+//     stock: {
+//         type: Number,
+//         default: 0,
+//     },
+//     // images: [{
+//     //     type: String,
+//     //     required: true,
+//     // }],
+//     images:{
+//         type:String
+//     },
+//     color: {
+//         type: String,
+//         default: 'black',
+//         enum: ['black', 'white', 'grey'],
+//     },
+//     size: {
+//         type: String,
+//         default: 'Small',
+//         enum: ['Small', 'Medium', 'Large'],
+//     },
+// }, { timestamps: true });
+
+// const Product = model('Product', productSchema);
+// module.exports = Product;
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const productSchema = new Schema({
     userId: {
@@ -30,12 +83,8 @@ const productSchema = new Schema({
         type: Number,
         default: 0,
     },
-    // images: [{
-    //     type: String,
-    //     required: true,
-    // }],
-    images:{
-        type:String
+    images: {
+        type: String,
     },
     color: {
         type: String,
@@ -46,6 +95,31 @@ const productSchema = new Schema({
         type: String,
         default: 'Small',
         enum: ['Small', 'Medium', 'Large'],
+    },
+    productType: {
+        type: String,
+        required: true,
+        enum: ['3DModelWithLogo', '3DModelWithoutLogo', '3DSoftwareWithLogo', '3DSoftwareWithoutLogo', 'RealModelWithSoftware'],
+    },
+    hasLogo: {
+        type: Boolean,
+        default: false,
+    },
+    softwareUrl: {
+        type: String,
+    },
+    version: {
+        type: String,
+    },
+    licenseKey: {
+        type: String,
+    },
+    systemRequirements: {
+        type: String,
+    },
+    isRealModelIncluded: {
+        type: Boolean,
+        default: false,
     },
 }, { timestamps: true });
 
