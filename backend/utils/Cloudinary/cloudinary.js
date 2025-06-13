68b4327ea763422929691244fe91617bb1fc5740
vikranth <vikranthka@gmail.com>
2025-06-13 18:43:22 +0530

    worked on cloudinary image

--- backend/utils/Cloudinary/cloudinary.js
+++ backend/utils/Cloudinary/cloudinary.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cloudinary  = require('cloudinary').v2;
 
 const { CLOUD_NAME, CLOUD_API_SECRET, CLOUD_API_KEY } = process.env;
-//cloudinary
+//cloudinary for images
 cloudinary.config({ 
   cloud_name: CLOUD_NAME, 
   api_key: CLOUD_API_KEY, 
