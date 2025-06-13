d3008e1f8fce810ff998944399167a80d813f12a
vikranth <vikranthka@gmail.com>
2025-06-13 18:42:53 +0530

    worked on the BE backend

--- backend/utils/Cloudinary/cloudinary.js
+++ backend/utils/Cloudinary/cloudinary.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const cloudinary  = require('cloudinary').v2;
 
 const { CLOUD_NAME, CLOUD_API_SECRET, CLOUD_API_KEY } = process.env;
-
+//cloudinary
 cloudinary.config({ 
   cloud_name: CLOUD_NAME, 
   api_key: CLOUD_API_KEY, 
   api_secret: CLOUD_API_SECRET 
 });
+
 
 module.exports = cloudinary;
 
